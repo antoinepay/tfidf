ca6e6175d4a7cd22ffbdf46253ad8a255bd163fa
benjaminsinturel <benjamin.sinturel@hec.edu>
2019-02-18 15:01:07 +0100

    MODIFY spark launcher

--- spark/launch_spark.sh
+++ spark/launch_spark.sh
@@ -3,29 +3,29 @@
 hdfs dfs -put corpus/0 /user/hadoop/corpus
 spark-submit --master yarn \
 --executor-memory 7g \
-—-num-executors 5 \
--—executor-cores 1 \
+--num-executors 5 \
+--executor-cores 1 \
 spark_instructions.py
 
 hdfs dfs -put corpus/1 /user/hadoop/corpus
 spark-submit --master yarn \
 --executor-memory 7g \
-—-num-executors 5 \
--—executor-cores 1 \
+--num-executors 5 \
+--executor-cores 1 \
 spark_instructions.py
 
 hdfs dfs -put corpus/2 /user/hadoop/corpus
 spark-submit --master yarn \
 --executor-memory 7g \
-—-num-executors 5 \
--—executor-cores 1 \
+--num-executors 5 \
+--executor-cores 1 \
 spark_instructions.py
 
 hdfs dfs -put corpus/3 /user/hadoop/corpus
 spark-submit --master yarn \
 --executor-memory 7g \
-—-num-executors 5 \
--—executor-cores 1 \
+--num-executors 5 \
+--executor-cores 1 \
 spark_instructions.py
 
 hdfs dfs -put corpus/4 /user/hadoop/corpus
